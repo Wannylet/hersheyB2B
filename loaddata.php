c0cac6c42438e25f6c841f5a5b634b4e9121150b
Wannylet <Josehrzcar.software@gmail.com>
2024-07-17 10:23:29 -0600

    Conexión FTP
    Ahora realiza la conexión y envío del archivo FTP

--- loaddata.php
+++ loaddata.php
@@ -18,8 +18,8 @@
 		
 		if (isset($_FILES['archivoProcesar']) && $_FILES['archivoProcesar']['error'] == 0) {
 
-			$nombreArchivo = (string) $_FILES['archivoProcesar']['name'];
-			$direccionArchivo = (string) __DIR__ . "\\" . $nombreArchivo;
+			$nombreArchivo = $_FILES['archivoProcesar']['name'];
+			$direccionArchivo = __DIR__ . "\\" . $nombreArchivo;
 
 			$archivoXML = simplexml_load_file($direccionArchivo);
 
@@ -35,6 +35,44 @@
 				
 				echo '<script language="javascript">alert("Se han insertado los datos exitosamente en la base de datos.");</script>';
 				
+				//- - - - - - - - - -
+
+				//variables – Nota – hacer una prueba primero en localhost.
+				$ftp_server = "31.170.167.212"; // Datos de X10 Hosting
+				$ftp_user_name = "u269761573"; // Usuario X10 Hosting
+				$ftp_user_pass = "tiendaRoot1"; // // Usuario X10 Hosting "cloudproject.cloud"
+				// carpeta destino en X10
+				$destination_file = "../Caleb/" . $nombreArchivo;
+                //Xml creado
+                $source_file = __DIR__ . "\\" . $nombreArchivo;
+
+				// conexión
+				$conn_id = ftp_connect($ftp_server);
+				
+				// login
+				$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+				
+				// conexión
+				if ((!$conn_id) || (!$login_result)) {
+					echo '<script language="javascript">alert("Conexión al FTP ' . $ftp_server . ' del usuario ' . $ftp_user_name . ' con errores.");</script>';
+				} else {
+					echo '<script language="javascript">alert("Conectado a ' . $ftp_server . ' del usuario ' . $ftp_user_name . '.");</script>';
+				}
+
+				$upload = ftp_put($conn_id, $destination_file, $source_file, FTP_BINARY);
+
+				// estado de envio
+				if ($upload) {
+					echo '<script language="javascript">alert("Archivo ' . $source_file . ' se ha subido exitosamente a ' . $ftp_server . ' en ' . $destination_file . '");</script>';
+				} else {
+					echo '<script language="javascript">alert("Archivo ' . $source_file . ' no se ha podido subir a ' . $ftp_server . ' en ' . $destination_file . '");</script>';
+				}
+
+				// cerramos la conexión.
+				ftp_close($conn_id);
+
+				//- - - - - - - - - -
+
 				echo '<script language="javascript">window.location="newone.php";</script>';
 			}
 		} else {
